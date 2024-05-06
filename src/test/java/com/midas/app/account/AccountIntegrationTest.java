7d4ed1ad81f4d736a2a4e649796328df85408eed
Ramon Sharif <jramun7@gmail.com>
2024-05-06 09:55:03 -0400

    [#none-UPDATE] Add a scenario test for creating account

--- src/test/java/com/midas/app/account/AccountIntegrationTest.java
+++ src/test/java/com/midas/app/account/AccountIntegrationTest.java
@@ -3,7 +3,6 @@ package com.midas.app.account;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -15,8 +14,6 @@ import com.midas.app.providers.external.stripe.StripePaymentProvider;
 import com.midas.app.repositories.AccountRepository;
 import com.midas.app.shared.AbstractTest;
 import com.midas.generated.model.CreateAccountDto;
-import com.midas.generated.model.UpdateAccountDto;
-import java.util.HashMap;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -41,7 +38,14 @@ public class AccountIntegrationTest extends AbstractTest {
     // * Implement default external dependencies actions (providers, etc)
     when(stripePaymentProvider.createAccount(
             any(com.midas.app.providers.payment.models.CreateAccount.class)))
-        .thenReturn(Account.builder().firstName("Ramon").lastName("Sharif").email("ramon@gamil.com").build());
+        .thenReturn(
+            Account.builder()
+                .firstName("Ramon")
+                .lastName("Sharif")
+                .providerType(ProviderType.STRIPE)
+                .providerId("default-provider-customer-id")
+                .email("ramon@gmail.com")
+                .build());
   }
 
   @Test
@@ -63,8 +67,7 @@ public class AccountIntegrationTest extends AbstractTest {
             jsonPath("$.firstName", is(createAccountDto.getFirstName())),
             jsonPath("$.lastName", is(createAccountDto.getLastName())),
             jsonPath("$.email", is(createAccountDto.getEmail())),
-            jsonPath("$.providerType", is(ProviderType.STRIPE.toString().toLowerCase())),
-            jsonPath("$.providerId", is("default-provider-customer-id")));
+            jsonPath("$.providerType", is(ProviderType.STRIPE.toString().toLowerCase())));
   }
 
   @Test
@@ -74,45 +77,5 @@ public class AccountIntegrationTest extends AbstractTest {
     var request = post("/accounts").contentType(MediaType.APPLICATION_JSON);
 
     mvc.perform(request).andDo(print()).andExpectAll(status().isBadRequest());
-  }
-
-  @Test
-  @Transactional
-  @DisplayName(
-      "Given updateAccountDto, When the PATCH /accounts called, Then account successfully updated")
-  void testUpdateUserAccountSuccessfully() throws Exception {
-    var createAccountDto =
-        new CreateAccountDto().firstName("Ramon").lastName("Sharif").email("ramon@gmail.com");
-    var updateAccountDto =
-        new UpdateAccountDto().firstName("Ramon2").lastName("Sharif2").email("ramon2@gmail.com");
-
-    // * Create new account
-    var createAccountRequest =
-        post("/accounts")
-            .content(objectMapper.writeValueAsString(createAccountDto))
-            .contentType(MediaType.APPLICATION_JSON);
-    var response =
-        objectMapper.readValue(
-            mvc.perform(createAccountRequest)
-                .andDo(print())
-                .andReturn()
-                .getResponse()
-                .getContentAsString(),
-            HashMap.class);
-    var newAccountId = response.get("id");
-
-    // * Update existed account
-    var request =
-        patch("/accounts/" + newAccountId)
-            .content(objectMapper.writeValueAsString(updateAccountDto))
-            .contentType(MediaType.APPLICATION_JSON);
-
-    mvc.perform(request)
-        .andDo(print())
-        .andExpectAll(
-            status().isOk(),
-            jsonPath("$.firstName", is(updateAccountDto.getFirstName())),
-            jsonPath("$.lastName", is(updateAccountDto.getLastName())),
-            jsonPath("$.email", is(updateAccountDto.getEmail())));
   }
 }
